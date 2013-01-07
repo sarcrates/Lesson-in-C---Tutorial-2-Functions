636e3c21eaa03c015efc8aded7f588e062c0aff2
Hana Sakamoto <sarcrates_dev@icloud.com>
2013-01-07 11:58:05 -0700

    Initial Commit

---
+++ Lesson in C - Tutorial 2 Functions/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lesson in C - Tutorial 2 Functions
+//
+//  Created by sarcrates on 1/7/13.
+//  Copyright © 2013 sarcrates. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
